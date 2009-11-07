c205edeae5bca4257f3605f8a4a1edc9e8cb69ca
Sean O'Connor <sean@seanoc.com>
2009-11-07 12:17:04 -0500

    Cleaned up bob_runner.py

--- bob_runner.py
+++ bob_runner.py
@@ -9,20 +9,3 @@ class BobRunner(DaemonRunner):
         self.daemon_context.signal_map = {
             signal.SIGTERM: self.app.stop
         }
-    
-    '''
-    def _start(self):
-        super(BobRunner, self)._start()
-        
-    def _restart(self):
-        super(BobRuner, self)._restart()
-    
-    def _stop(self):
-        self.app.stop()
-        super(BobRunner, self)._stop()
-        
-    action_funcs = {
-        'start': _start,
-        'stop': _stop,
-        'restart': _restart,
-        }'''
