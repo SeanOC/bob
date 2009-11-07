67dfa9f76c835a9a3091e0230cda101ee62a5119
Sean O'Connor <sean@seanoc.com>
2009-11-07 12:15:12 -0500

    Added a template settings file.

---
+++ settings.template.py
@@ -0,0 +1,22 @@
+CAMPFIRE_LOGIN = 'user@domain.com'
+CAMPFIRE_PASSWORD = 'FIXME'
+CAMPFIRE_DOMAIN = 'domain'
+CAMPFIRE_SSL = True
+CAMPFIRE_ROOM = 'My Room'
+
+SVN_SERVER = 'http://svn.domain.com/'
+SVN_WATCH_PATHS = (
+    '/project1',
+    '/project2',
+    '/other_project/things',
+)
+
+DB_FILE = '/path/to/data/dir/bob.db'
+
+COMMIT_URL = 'http://trac.domain.com/changeset/%d'
+
+
+LOG_FILE = 'bob.log'
+ERROR_FILE = 'bob.error'
+LOCK_FILE = '/var/run/bob.lock'
+LOCK_TIMEOUT = 30
